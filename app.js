05d8e539a1bc8abe38a4f4228baea1efd9f3e9bf
flagos <flagospub@gmail.com>
2019-05-02 23:39:04 +0200

    Add color in recipe list

--- app.js
+++ app.js
@@ -21,12 +21,11 @@ require('electron').ipcRenderer.on('recipes-list', (event, message) => {
 	$("#collection-search-results").append(
 	    `
 <li class="collection-item avatar">
-    <i class="material-icons circle red">play_arrow</i>
+    <i class="material-icons circle" style="background-color:${Brauhaus.srmToCss(recipe[0].color)}"></i>
     <span class="title">${recipe[0].name}</span>
     <p>${recipe[0].style.name}<br>
-     ${recipe[0].abv.toFixed(1)}° - ${recipe[0].ibu.toFixed(0)} IBU
+     ${recipe[0].abv.toFixed(1)}% - ${recipe[0].ibu.toFixed(0)} IBU
     </p>
-    <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
 </li>
 `);
     });
