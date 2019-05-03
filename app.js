99b127913b0ad3a0a7e0ddbb3c23252cb07b8be5
flagos <flagospub@gmail.com>
2019-05-03 18:28:31 +0200

    first implementation of search bar

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
 });
 
+var recipes = [];
+var search_txts = [];
 
 require('electron').ipcRenderer.on('recipes-list', (event, message) => {
     console.log(message);
-    var recipes = [];
+
+    recipes = [];
 
     message.forEach(function(item){
 	r = Brauhaus.Recipe.fromBeerXml(item["content"]);
@@ -17,6 +20,7 @@ require('electron').ipcRenderer.on('recipes-list', (event, message) => {
 
     console.log(recipes);
 
+    // construct list
     recipes.forEach(function(recipe){
 	$("#collection-search-results").append(
 	    `
@@ -29,4 +33,46 @@ require('electron').ipcRenderer.on('recipes-list', (event, message) => {
 </li>
 `);
     });
+
+    recipes.forEach(function(recipe){
+	// construct search text
+	var txtValues;
+	recipe = recipe[0];
+	txtValues = [recipe.name, recipe.style.name];
+	recipe.fermentables.forEach(function(f){
+	    txtValues.push(f.name);
+	});
+	recipe.spices.forEach(function(s){ // spices are actually hops
+	    txtValues.push(s.name);
+	});
+	recipe.yeast.forEach(function(y){
+	    txtValues.push(y.name + y.type);
+	});
+
+	search_txts.push(txtValues.join('/'));
+    });
+
+
 });
+
+
+function search_recipes() {
+
+    var input, filter, ul, li, a, i, txtValue;
+
+    input = document.getElementById('search');
+    filter = input.value.toUpperCase();
+
+    ul = document.getElementById("collection-search-results");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < recipes.length; i++) {
+	txtValue = search_txts[i];
+	if (txtValue.toUpperCase().indexOf(filter) > -1) {
+	    li[i].style.display = "";
+	} else {
+	    li[i].style.display = "none";
+	}
+    }
+}
